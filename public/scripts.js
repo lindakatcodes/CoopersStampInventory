35f46a33c2a3747321aa88d841fc01839b16a063
Lindakat <lindakt16@gmail.com>
2019-11-12 00:40:08 -0600

    Data is officially working and in the right spots!

--- public/scripts.js
+++ public/scripts.js
@@ -7,11 +7,18 @@ const data = fetch('http://localhost:8080/getdata')
 .then(res => {
     const initData = res;
 
-    for (let i = 1, row; row = stampTable.rows[i]; i++) {
+    // for each db item, find it's equal DOM item and set the value
+    for (let i = 0; i < initData.length; i++) {
+        const name = initData[i].name;
+        const nameDiv = document.getElementById(name);
+        nameDiv.textContent = initData[i].qty;
+    }
+
+    // for (let i = 1, row; row = stampTable.rows[i]; i++) {
         
-        for (let j = 1, col; col = row.cells[j]; j++) {
-          let currId = col.id;
+    //     for (let j = 1, col; col = row.cells[j]; j++) {
+    //       let currId = col.id;
           
-        }  
-     }
+    //     }  
+    //  }
 })
